7d0ce655f904593e9e912e3f9c8bb30c1e3db466
vaishnavipatl <patilvaishnavi2510@gmail.com>
2024-12-12 18:25:02 +0530

    upload new file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useEffect ,useState } from 'react';
 function App() {
+const[activekey,setactivekey]=useState('')
+useEffect(()=>{
+      document.addEventListener('keydown',(event)=>{
+      playsound(event.key.toUpperCase());
+      })
+},[])
+const drumpads = [
+  {
+    keyCode: 81,
+    text: "Q",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    keyCode: 87,
+    text: "W",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+
+
+  },
+  {
+    keyCode: 69,
+    text: "E",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    keyCode: 65,
+    text: "A",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    keyCode: 83,
+    text: "S",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    keyCode: 68,
+    text: "D",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  },
+  {
+    keyCode: 90,
+    text: "Z",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    keyCode: 88,
+    text: "X",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    keyCode: 67,
+    text: "C",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  }
+];
+
+function playsound(selector){
+     const audio=document.getElementById(selector)
+     console.log(audio)
+     audio.play();
+     setactivekey(selector)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div id='drum-machine'>
+           <div id='display'>{activekey}</div>
+           <div className='drum-pads'>
+             {
+                drumpads.map((drumpad)=>
+                (
+                   <div  
+                     key={drumpad.src}  
+                     onClick={()=> playsound(drumpad.text)}
+                     className='drum-pad' 
+                     id={drumpad.src}>
+                     {drumpad.text}
+                       <audio className='clip' 
+                        id={drumpad.text} 
+                        src={drumpad.src} type="audio/mp3" 
+                        preload="auto" 
+                        onError={(e) => console.error("Audio error:", e)}
+                       ></audio>
+                    </div>
+                ))
+              }
+            </div>
+       </div>
     </div>
   );
 }
